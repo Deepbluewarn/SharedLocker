5d249f189d12bfe8e52691f942faffb0f9ca2cd8
bluewarn <n5lp97@gmail.com>
2024-07-23 17:57:52 +0900

    feat: 공유중인 보관함 취소시 양도자를 선택하는 Dialog를 추가.

---
+++ src/styles/home.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const HomeStyles = StyleSheet.create({
+    AssigneeDialogContainer: {
+        display: 'flex',
+        gap: 8,
+        marginTop: 8
+    },
+    AssigneeList: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8
+    }
+})
