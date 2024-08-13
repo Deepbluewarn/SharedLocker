30681fc964256c15584f7635031972147c7ff839
bluewarn <n5lp97@gmail.com>
2024-08-13 19:39:29 +0900

    feat: 회원 탈퇴 기능 구현

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,18 +1,54 @@
 import Layout from "@/components/Layout";
 import { SettingStackParamList, SettingStackScreenProps } from "@/navigation/types";
 import authAPI from "@/network/auth/api";
-import { ILogout } from "@/types/api/auth";
+import { IDelete, ILogout } from "@/types/api/auth";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Alert } from "react-native";
 import { Appbar, Button, Text } from "react-native-paper";
 import { Profile } from "./Profile";
 import LockerManagement from "./LockerManagement";
+import Toast from "react-native-toast-message";
+import { IServerErrorResponse } from "@/types/api";
 
 export default function Settings(props: SettingStackScreenProps<'Settings'>) {
+  const queryClient = useQueryClient();
+  const removeCache = () => {
+    queryClient.setQueryData(['auth'], null);
+  };
   const { refetch } = useQuery<ILogout>(['auth'], () => authAPI().signOut(), {
     enabled: false,
     retry: false,
+  });
+
+  const deleteMutation = useMutation<IDelete, IServerErrorResponse<string>>({
+    mutationFn: () => {
+      return authAPI().deleteAccount();
+    },
+    onSuccess: deleteData => {
+      if (!deleteData) {
+        return;
+      }
+
+      const _data = deleteData.data;
+
+      if (_data.success) {
+        removeCache();
+        
+        Toast.show({
+          type: 'success',
+          text1: '성공',
+          text2: _data?.message,
+        });
+      }
+    },
+    onError(error: IServerErrorResponse<string>) {
+      console.log('error.response.data.message: ', error.response.data.message)
+      Toast.show({
+        type: 'error',
+        text2: error.response.data.message
+      });
+    },
   });
 
   const beforeLogout = () => {
@@ -34,6 +70,26 @@ export default function Settings(props: SettingStackScreenProps<'Settings'>) {
       ],
     )
   }
+
+  const beforeQuit = () => {
+    Alert.alert(
+      '회원 탈퇴',
+      '회원 탈퇴 하시겠습니까?',
+      [
+        {
+          text: '취소',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: '확인',
+          onPress: () => {
+            deleteMutation.mutate()
+          },
+        },
+      ],
+    )
+  }
   
   return (
     <>
@@ -49,6 +105,14 @@ export default function Settings(props: SettingStackScreenProps<'Settings'>) {
           buttonColor="#db4455"
         >
           로그아웃
+        </Button>
+
+        <Button
+          mode='contained-tonal'
+          onPress={() => { beforeQuit() }}
+          buttonColor="#db4455"
+        >
+          회원 탈퇴
         </Button>
       </Layout>
     </>
